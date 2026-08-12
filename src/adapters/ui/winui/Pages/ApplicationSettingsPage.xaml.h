30554e635fae6f147bad8ea871f5bc805c5e87a1
CoolPlume <75199132+CoolPlume@users.noreply.github.com>
2026-08-12 19:45:55 +0800

    feat: implement application update recovery lifecycle

--- src/adapters/ui/winui/Pages/ApplicationSettingsPage.xaml.h
+++ src/adapters/ui/winui/Pages/ApplicationSettingsPage.xaml.h
@@ -1,12 +1,37 @@
 #pragma once
 
+#include <memory>
+
 #include "Pages/ApplicationSettingsPage.g.h"
+#include "azzs/application/workbench.hpp"
 
 namespace winrt::Azzs::Ui::Pages::implementation {
 
 struct ApplicationSettingsPage
     : ApplicationSettingsPageT<ApplicationSettingsPage> {
   ApplicationSettingsPage();
+
+  void bind(std::shared_ptr<azzs::application::Workbench> workbench);
+  void OnApplicationUpdateCommandClick(
+      Microsoft::UI::Xaml::Controls::Button const&,
+      Microsoft::UI::Xaml::RoutedEventArgs const&);
+  void OnApplicationUpdateRetryClick(
+      Microsoft::UI::Xaml::Controls::Button const&,
+      Microsoft::UI::Xaml::RoutedEventArgs const&);
+  void OnApplicationUpdateRestoreClick(
+      Microsoft::UI::Xaml::Controls::Button const&,
+      Microsoft::UI::Xaml::RoutedEventArgs const&);
+  void OnApplicationUpdateManualClick(
+      Microsoft::UI::Xaml::Controls::Button const&,
+      Microsoft::UI::Xaml::RoutedEventArgs const&);
+  void OnApplicationUpdateDiagnosticClick(
+      Microsoft::UI::Xaml::Controls::Button const&,
+      Microsoft::UI::Xaml::RoutedEventArgs const&);
+
+ private:
+  void project(azzs::application::UpdateSnapshot const& snapshot);
+
+  std::shared_ptr<azzs::application::Workbench> workbench_;
 };
 
 }  // namespace winrt::Azzs::Ui::Pages::implementation
